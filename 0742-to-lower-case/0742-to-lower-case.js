df443dd9ff31b06fcaf4575e6c252d43d5ed5067
AbinPrasanth <abinprasanth007@gmail.com>
2025-01-18 09:31:59 +0530

    Time: 0 ms (100%), Space: 48.8 MB (78.31%) - LeetHub

---
+++ 0742-to-lower-case/0742-to-lower-case.js
@@ -0,0 +1,10 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var toLowerCase = function(s) {
+  const low = s.toLowerCase();
+  return low;
+  console.log(low);  
+};
+var result = toLowerCase("Abin");
